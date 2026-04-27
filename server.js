94b301c8be36b066562f8fe6361e219ac3806421
yaca007 <vicacosta@gmail.com>
2026-04-27 09:26:20 -0300

    paso de gemini 1.5 a 2.5

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.post("/presentation", async (req, res) => {
 
     // 1. Gemini estructura el contenido
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      model: "gemini-2.5-flash",
 
     });
 
